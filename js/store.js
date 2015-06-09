5240ecbbaf8fe77f58dfb53752784598924428ca
Alec Arme & Ben Cornelis <student@epicodus.com>
2015-06-09 10:42:52 -0700

    Add additional modeling - post sorting, post detail/editing

---
+++ js/store.js
@@ -0,0 +1,64 @@
+var posts  = [
+  {
+    id: 1,
+    title: "Names",
+    body: 'Igor: Dr. Frankenstein...
+            Dr. Frederick Frankenstein: "Fronkensteen."
+            Igor: Youre putting me on.
+            Dr. Frederick Frankenstein: No, its pronounced "Fronkensteen."
+            Igor: Do you also say "Froaderick"?
+            Dr. Frederick Frankenstein: No... "Frederick."
+            Igor: Well, why isnt it "Froaderick Fronkensteen"?
+            Dr. Frederick Frankenstein: It isnt; its "Frederick Fronkensteen."
+            Igor: I see.
+            Dr. Frederick Frankenstein: You must be Igor.
+            [He pronounces it ee-gor]
+            Igor: No, its pronounced "eye-gor."
+            Dr. Frederick Frankenstein: But they told me it was "ee-gor."
+            Igor: Well, they were wrong then, werent they?'
+  },
+
+  {
+    id: 2,
+    title: "Dad",
+    body: 'Igor: You know, Ill never forget my old dad. When these things would happen to him... the things hed say to me.
+            Dr. Frederick Frankenstein: What did he say?
+            Igor: "What the hell are you doing in the bathroom day and night? Why dont you get out of there and give someone else a chance?'
+ },
+
+  {
+    id: 3,
+    title: "Werewolf!",
+    body: 'Inga: Werewolf!
+            Dr. Frederick Frankenstein: Werewolf?
+            Igor: There.
+            Dr. Frederick Frankenstein: What?
+            Igor: There, wolf. There, castle.
+            Dr. Frederick Frankenstein: Why are you talking that way?
+            Igor: I thought you wanted to.
+            Dr. Frederick Frankenstein: No, I dont want to.
+            Igor: [shrugs] Suit yourself. Im easy.'
+  }
+
+]
+
+var comments = [
+  {
+    id: 1,
+    postId: 3,
+    text: 'Best movie ever!'
+  },
+
+  {
+    id: 2,
+    postId: 2,
+    text: "Igor's the man!"
+  },
+
+  {
+    id: 3,
+    postId: 1,
+    text: "I love this movie!"
+  }
+
+]
